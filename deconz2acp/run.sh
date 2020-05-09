eb27006e257a3f422fd7cb6a31b82e327feb63f6
Ian Lewis <ijl20@cam.ac.uk>
2020-05-09 14:14:49 +0100

    created run.sh suitable for acp_prod/crontab

--- deconz2acp/run.sh
+++ deconz2acp/run.sh
@@ -2,13 +2,11 @@
 
 ##################################
 # run.sh
-# Used to start acp_decoders
 ##################################
 
 # Find the directory this script is being run from
-# typically "/home/acp_prod/acp_local_mqtt/acp_decoders"
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-$SCRIPT_DIR/acp_decoders/run.sh >>/var/log/acp_prod/acp_decoders.log 2>/var/log/acp_prod/acp_decoders.err
+nohup $SCRIPT_DIR/run_dev.sh >>/dev/null 2>/var/log/acp_prod/deconz2acp.err & disown
 
