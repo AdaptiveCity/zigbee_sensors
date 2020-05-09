3040488ad05c2c57d6d51a72c31e67cf57305ddc
Ian Lewis <ijl20@cam.ac.uk>
2020-05-09 14:34:44 +0100

    run.sh improved

--- deconz2acp/run.sh
+++ deconz2acp/run.sh
@@ -8,5 +8,17 @@
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-nohup $SCRIPT_DIR/run_dev.sh >>/dev/null 2>/var/log/acp_prod/deconz2acp.err & disown
+cd $SCRIPT_DIR
+
+pid=$(pgrep -f "python3 deconz2acp.py")
+
+if [ $? -eq 0 ]
+then
+    echo $(date '+%s') $SCRIPT_DIR/run_dev.sh FAIL: deconz2acp.py already running as PID $pid >>/var/log/acp_prod/deconz2acp.err
+    exit 1
+else
+    source venv/bin/activate
+    nohup python3 deconz2acp.py >>/dev/null 2>>/var/log/acp_prod/deconz2acp.err & disown
+    exit 0
+fi
 
